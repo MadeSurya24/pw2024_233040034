c87737669d7dbf0007875873fcf6c49c5feda103
MadeSurya24 <sai007tdw@gmail.com>
2024-02-17 22:04:19 +0700

    .

--- tugas/tugas1/1a.php
+++ tugas/tugas1/1a.php
@@ -1,20 +1,7 @@
 <?php
     $topi = "Topi";
     $bundar = "Bundar";
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-   <h2> 
-    <em>
-        <?php echo '"'. " " . $topi . " " . "saya" . " " . $bundar . ',' . " " . $bundar . " " . $topi . " " . "saya." . " " . '"';?>
-    </em>
-   </h2> 
-</body>
-</html>
+    echo "<h2><em>" . '"' . $topi . " " . "saya" . " " . $bundar . ',' . " " . $bundar . " " . $topi . " " . "saya." . '"';
+
+?>
--- tugas/tugas1/1b.php
+++ tugas/tugas1/1b.php
@@ -1,22 +1,15 @@
 <?php 
+
     $npm = "34";
+
+    echo "Aku adalah angka <strong> $npm </strong> <br>";
+    $npm *= 5;
+    echo "Jika aku dikali 5, maka aku sekarang menjadi <strong> $npm </strong> <br>";
+    $npm /= 2;
+    echo "Jika aku dibagi 2, maka aku sekarang menjadi <strong> $npm </strong> <br>";
+    $npm += 75;
+    echo "Jika aku ditambah 75, maka aku sekarang menjadi <strong> $npm </strong> <br>";
+    $npm -= 20;
+    echo "Jika aku dikurang 20, maka aku sekarang menjadi <strong> $npm </strong>";
+   
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <p>
-        Aku adalah angka <strong> <?php echo $npm; ?> </strong><br>
-        Jika aku dikali 5, maka aku sekarang menjadi <strong> <?php echo $npm * 5; ?> </strong><br>
-        Jika aku dibagi 2, maka aku sekarang menjadi <strong> <?php echo $npm / 2; ?> </strong><br>
-        Jika aku ditambah 75, maka aku sekarang menjadi <strong> <?php echo $npm + 75; ?> </strong><br>
-        Jika aku dikurang 20, maka aku sekarang menjadi <strong> <?php echo $npm - 20; ?> </strong>
-    </p>
-</body>
-</html>
-
